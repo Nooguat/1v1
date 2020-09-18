e07444c69c95417cacd1f87c2887a999fa11bd77
nooguat <jjnooguatadr@gmail.com>
2020-09-18 23:32:01 +0200

    Ending dices impl

--- LeoAlexisVincent/main/Des.py
+++ LeoAlexisVincent/main/Des.py
@@ -1,3 +1,4 @@
+from random import randint
 class Des():
     def __init__(self, n_d8, n_d4):
         self.dice_type = ["d8", "d4", "d2"]
@@ -6,8 +7,17 @@ class Des():
     
     def ThrowDice(d_type) -> int:
         if d_type = self.dice_type[0]:
-            pass
+            if self.d8 == 0:
+                print("Vous auriez dû vous souvenir que vous aviez utilisé votre dernier d8. Dommage ! ")
+                return 0
+            self.d8 -= self.d8
+            return randint(1,8)
         if d_type = self.dice_type[1]:
-            pass
-        if d_type = self.dice_type[2]:
-            pass
+            if self.d4 == 0:
+                print("Vous auriez dû vous souvenir que vous aviez utilisé votre dernier d4. Dommage ! ")
+                return 0
+            self.d4 -= self.d4
+            return randint(1,4)
+        print("Vous avez utilisé un d2, et ca tombe bien, on en a plein ! ")
+        return randint(1,2)
+        
